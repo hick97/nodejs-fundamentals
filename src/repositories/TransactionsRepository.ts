6786a24557e8e156e69a7cac2971561f06110a99
Henrique Augusto <hick_97@hotmail.com>
2020-04-30 13:31:32 -0300

    feat: challenge 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public checkIfOutcomeIsValid(outcome: number): boolean {
+    const balance = this.getBalance();
+
+    if (balance.total - outcome < 0) {
+      return false;
+    }
+
+    return true;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+
+    const incomeArray = this.transactions.filter(t => t.type === 'income');
+    const outcomeArray = this.transactions.filter(t => t.type === 'outcome');
+
+    const totalIncome = incomeArray.map(i => i.value).reduce(reducer, 0);
+    const totalOutcome = outcomeArray.map(o => o.value).reduce(reducer, 0);
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
